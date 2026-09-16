616e2ea063a32f53b96ddaa3c7f7843b2ab9ff16
Javer Hossain <imjaberhossain@gmail.com>
2026-09-16 16:17:38 +0600

    Test1

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS messages (
+                                        id BIGSERIAL PRIMARY KEY,
+                                        sender_id BIGINT,
+                                        receiver_id BIGINT,
+                                        content TEXT,
+                                        timestamp TIMESTAMP
+)
